7401499399ab83eac82ab24db12734e3829c6250
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-09 22:58:06 -0300

    Criando Drawer Customizado

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
-
   final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-
     return PageView(
       physics: NeverScrollableScrollPhysics(),
       children: [
-        HomeTab(),
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(),
+        )
       ],
     );
   }
