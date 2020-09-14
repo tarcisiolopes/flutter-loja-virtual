4e801e83206b3a617045f5432f26a5977476bd3c
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-14 15:37:46 -0300

    Criando a Tile da Grid

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,15 +1,54 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
 class ProductTile extends StatelessWidget {
-
   final String type;
-  final ProductData data;
+  final ProductData product;
 
-  ProductTile(this.type, this.data);
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return InkWell(
+      child: Card(
+        child: type == "grid"
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            product.titulo,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "R\$ ${product.preco.toStringAsFixed(2)}",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            : Row(),
+      ),
+    );
   }
 }
