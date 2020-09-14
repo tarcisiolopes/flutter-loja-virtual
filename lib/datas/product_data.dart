1c090423a19c9631358b6ed07194d1a29d2221a6
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-14 14:57:46 -0300

    Estruturando os dados do Produto

---
+++ lib/datas/product_data.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductData {
+
+  String id;
+
+  String titulo;
+  String descricao;
+  double preco;
+  List images;
+  List tamanhos;
+
+  ProductData.fromDocument(DocumentSnapshot snapshot){
+    id = snapshot.documentID;
+    titulo = snapshot.data["titulo"];
+    descricao = snapshot.data["descricao"];
+    preco = snapshot.data["preco"] + 0.0;
+    images = snapshot.data["images"];
+    tamanhos = snapshot.data["tamanhos"];
+  }
+
+}
