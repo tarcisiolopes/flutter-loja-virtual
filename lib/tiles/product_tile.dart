9b02c60f1bb9467e4a20456cd53a8d5ecb01bd7d
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-14 15:47:50 -0300

    Criando a Tile da Lista

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
+import 'package:loja_virtual/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
   final String type;
@@ -11,6 +12,11 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context)=>ProductScreen(product))
+        );
+      },
       child: Card(
         child: type == "grid"
             ? Column(
@@ -36,10 +42,9 @@ class ProductTile extends StatelessWidget {
                           Text(
                             "R\$ ${product.preco.toStringAsFixed(2)}",
                             style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontSize: 17.0,
-                              fontWeight: FontWeight.bold
-                            ),
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.bold),
                           )
                         ],
                       ),
@@ -47,7 +52,40 @@ class ProductTile extends StatelessWidget {
                   )
                 ],
               )
-            : Row(),
+            : Row(
+          children: [
+            Flexible(
+              flex: 1,
+              child: Image.network(
+                product.images[0],
+                fit: BoxFit.cover,
+                height: 250.0,
+              ),
+            ),
+            Flexible(
+              flex: 1,
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        product.titulo,
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      Text(
+                        "R\$ ${product.preco.toStringAsFixed(2)}",
+                        style: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 17.0,
+                            fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                )
+            )
+          ],
+        ),
       ),
     );
   }
