78f2bc17c78736fa8eb9c7c562da93bc0a20e4dc
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-16 10:08:49 -0300

    Recuperando os produtos do Firebase

--- lib/tiles/cart_tile.dart
+++ lib/tiles/cart_tile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/cart_product.dart';
 import 'package:loja_virtual/datas/product_data.dart';
+import 'package:loja_virtual/models/cart_model.dart';
 
 class CartTile extends StatelessWidget {
   final CartProduct cartProduct;
@@ -52,17 +53,23 @@ class CartTile extends StatelessWidget {
                       IconButton(
                         icon: Icon(Icons.remove),
                         color: Theme.of(context).primaryColor,
-                        onPressed: cartProduct.quantity > 1 ? (){} : null,
+                        onPressed: cartProduct.quantity > 1 ? (){
+                          CartModel.of(context).decProduct(cartProduct);
+                        } : null,
                       ),
                       Text(cartProduct.quantity.toString()),
                       IconButton(
                         icon: Icon(Icons.add),
-                        onPressed: (){},
+                        onPressed: (){
+                          CartModel.of(context).incProduct(cartProduct);
+                        },
                       ),
                       FlatButton(
                         child: Text("Remover"),
                         textColor: Colors.grey[500],
-                        onPressed: (){},
+                        onPressed: (){
+                          CartModel.of(context).removeCardItem(cartProduct);
+                        },
                       )
                     ],
                   ),
