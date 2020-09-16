7ac12de3b2a40459a93a5fd5cade9e5aad69b23f
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-16 09:41:17 -0300

    Recuperando os dados do produto

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
+import 'package:loja_virtual/tiles/cart_tile.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -69,6 +70,18 @@ class CartScreen extends StatelessWidget {
                 child: Text("Nenhum produto no carrinho!",
                 style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,),
+              );
+            } else {
+              return ListView(
+                children: [
+                  Column(
+                    children: model.products.map(
+                        (product){
+                          return CartTile(product);
+                        }
+                    ).toList(),
+                  )
+                ],
               );
             }
           },
