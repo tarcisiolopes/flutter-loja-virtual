f8fa25543105410c4b238084198e84fbed00266f
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-16 10:29:29 -0300

    Aplicando um Cupom de Desconto

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -9,6 +9,9 @@ class CartModel extends Model {
 
   List<CartProduct> products = [];
 
+  String couponCode;
+  int discountPercentage = 0;
+
   bool isLoading = false;
 
   CartModel(this.user){
@@ -70,5 +73,10 @@ class CartModel extends Model {
     products = query.documents.map((doc) => CartProduct.fromDocument(doc)).toList();
 
     notifyListeners();
+  }
+
+  void setCoupon(String couponCode, int discountPercentage){
+    this.couponCode = couponCode;
+    this.discountPercentage = discountPercentage;
   }
 }
