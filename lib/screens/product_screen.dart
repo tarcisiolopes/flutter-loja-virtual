903809bf8f19ed29610453eec492e6749bc7f646
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-16 04:10:26 -0300

    Criando tela do carrinho

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -5,6 +5,7 @@ import 'package:loja_virtual/datas/cart_product.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
+import 'package:loja_virtual/screens/cart_screen.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -120,6 +121,9 @@ class _ProductScreenState extends State<ProductScreen> {
                               cartProduct.category = product.category;
 
                               CartModel.of(context).addCardItem(cartProduct);
+
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => CartScreen()));
                             } else {
                               Navigator.of(context).push(MaterialPageRoute(
                                   builder: (context) => LoginScreen()));
