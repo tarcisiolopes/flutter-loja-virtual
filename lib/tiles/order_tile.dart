2c6d48a250eb001e4d0e9b428d3f565f20d2c24a
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-17 11:47:12 -0300

    Acompanhando status do pedido

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -26,14 +26,44 @@ class OrderTile extends StatelessWidget {
                   child: CircularProgressIndicator(),
                 );
               else {
+                int status = snapshot.data["status"];
+
                 return Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Código do pedido: ${snapshot.data.documentID}",
-                    style: TextStyle(fontWeight: FontWeight.bold),),
-                    SizedBox(height: 4.0,),
                     Text(
-                      _buildProductsText(snapshot.data)
+                      "Código do pedido: ${snapshot.data.documentID}",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 4.0,
+                    ),
+                    Text(_buildProductsText(snapshot.data)),
+                    SizedBox(
+                      height: 4.0,
+                    ),
+                    Text(
+                      "Status do pedido:",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 4.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        _buildCircle("1", "Preparação", status, 1),
+                        Container(
+                          height: 1.0,
+                          width: 40.0,
+                          color: Colors.grey[500],
+                        ),
+                        _buildCircle("2", "Transporte", status, 2),
+                        Container(
+                          height: 1.0,
+                          width: 40.0,
+                          color: Colors.grey[500],
+                        ),
+                        _buildCircle("3", "Entrega", status, 3),
+                      ],
                     )
                   ],
                 );
@@ -43,12 +73,54 @@ class OrderTile extends StatelessWidget {
     );
   }
 
-  String _buildProductsText(DocumentSnapshot snapshot){
+  String _buildProductsText(DocumentSnapshot snapshot) {
     String text = "Descrição:\n";
-    for(LinkedHashMap p in snapshot.data["products"]){
-      text += "${p["quantity"]} x ${p["product"]["titulo"]} (R\$ ${p["product"]["preco"].toStringAsFixed(2)})\n";
+    for (LinkedHashMap p in snapshot.data["products"]) {
+      text +=
+          "${p["quantity"]} x ${p["product"]["titulo"]} (R\$ ${p["product"]["preco"].toStringAsFixed(2)})\n";
     }
     text += "Total: R\$ ${snapshot.data["totalPrice"].toStringAsFixed(2)}";
     return text;
+  }
+
+  Widget _buildCircle(
+      String title, String subtitle, int status, int thisStatus) {
+    Color backColor;
+    Widget child;
+
+    if (status < thisStatus) {
+      backColor = Colors.grey[500];
+      child = Text(
+        title,
+        style: TextStyle(color: Colors.white),
+      );
+    } else if (status == thisStatus) {
+      backColor = Colors.blue;
+      child = Stack(
+        alignment: Alignment.center,
+        children: [
+          Text(
+            title,
+            style: TextStyle(color: Colors.white),
+          ),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          )
+        ],
+      );
+    } else {
+      backColor = Colors.green;
+      child = Icon(Icons.check, color: Colors.white,);
+    }
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 20.0,
+          backgroundColor: backColor,
+          child: child,
+        ),
+        Text(subtitle)
+      ],
+    );
   }
 }
